55f3c4599fa15063949601acdad3759d225af000
Olha36 <olhakucheruk36@gmail.com>
2023-11-28 15:40:32 +0200

    add adaptive

---
+++ 2 semester/homework27/script.js
@@ -0,0 +1,6 @@
+const portfolio = document.querySelector('.portfolio');
+const container = document.querySelector('.container');
+
+portfolio.addEventListener('click', function() {
+    container.classList.add('active');т
+})
