144c0d604314dfcd066ffbe05a804c9e2ba81e00
Olha36 <olhakucheruk36@gmail.com>
2024-02-23 15:07:28 +0200

    add examples

---
+++ 4semester/lesson2/script.js
@@ -0,0 +1,41 @@
+const vanillaOutput = document.querySelector(".output.vanilla");
+const throttledOutput = document.querySelector(".output.throttled");
+const debouncedOutput = document.querySelector(".output.debounced");
+const eventCounter = {
+  vanilla: 0,
+  throttled: 0,
+  debounced: 0
+};
+
+
+
+
+
+document.addEventListener("scroll", () => {
+  eventCounter.vanilla += 1;
+  vanillaOutput.textContent = eventCounter.vanilla;
+});
+
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    eventCounter.throttled += 1;
+    throttledOutput.textContent = eventCounter.throttled;
+  }, 300)
+);
+
+document.addEventListener(
+  "scroll",
+  _.debounce(() => {
+    eventCounter.debounced += 1;
+    debouncedOutput.textContent = eventCounter.debounced;
+  }, 300)
+)
+_.forEach([1, 2, 3, 6, 5], function(value) {
+  console.log(value);
+});
+
+
+console.log(_.includes([1, 2, 3], 1, 0));
+// => false
