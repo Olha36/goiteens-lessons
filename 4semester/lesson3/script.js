55d77902fc7262a16ff511c87db8a46ee58b742e
Olha36 <olhakucheruk36@gmail.com>
2024-03-04 19:54:12 +0200

    add

--- 4semester/lesson3/script.js
+++ 4semester/lesson3/script.js
@@ -19,68 +19,33 @@ const fruitItems = document.querySelectorAll('.fruits-list li');
 const imgSelector = document.querySelectorAll('.fruits-list li img');
 const fruitNameSelector = document.querySelectorAll('.fruits-list li h3');
 
-//1. Створіть функцію, яка буде приймати два параметри: el (елемент li) та isIntersecting (булеве значення, що вказує, 
-// чи перебуває елемент в зоні видимості):
-
-// const animate = (el, isIntersecting) => {
-//   let element = el.querySelector(imgSelector);
-
-//   if(isIntersecting) {
-//     element.classList.toggle('active');
-//   }
-// }
-// //2 Оновіть callback-функцію IntersectionObserver, щоб використовувати animate для анімації елементів li:
-// let observer = (entries => {
-//   entries.forEach((entry) => {
-//     let entryTarget = entry.target;
-//     animate(entryTarget, entry.isIntersecting)
-//   })
-// })
-
-
-// //3 Створіть екземпляр IntersectionObserver з callback-функцією та параметрами:
-
-// let observerParameters = new IntersectionObserver(observer, {
-//   threshold: 0.5,
-// })
-// //4 Використовуйте observer для спостереження за всіма елементами li:
-
-
-
 const animate = (el, isIntersecting) => {
-  let img = el.querySelector('img');
+  let element = el.querySelector('img');
   let h3 = el.querySelector('h3')
-  
+  console.log(element);
+
   if(isIntersecting) {
-    // img.classList.toggle('active');
-    h3.style.color = 'red';
-    let images = ['./img/banana.jpg', './img/pineapple.jpg', './img/watermelon.jpg', './img/cherries.jpg', './img/kiwi.jpg', './img/strawberry.jpg']; // Array of image URLs
-    let randomIndex = Math.floor(Math.random() * images.length); // Get a random index from the images array
-    img.src = images[randomIndex]; 
+    element.classList.toggle("active");
+    h3.style.backgroundColor = 'red'
   }
 }
-//2 Оновіть callback-функцію IntersectionObserver, щоб використовувати animate для анімації елементів li:
-let observerCallback = (entries => {
-  entries.forEach((entry) => {
+
+const observer = (entries) => {
+  entries.forEach(entry => {
     let entryTarget = entry.target;
     animate(entryTarget, entry.isIntersecting)
   })
-})
+  console.log(entries);
+}
 
-
-// //3 Створіть екземпляр IntersectionObserver з callback-функцією та параметрами:
-
-let observerParameters = new IntersectionObserver(observerCallback, {
+const observerCallback = new IntersectionObserver(observer, {
   threshold: 0.5,
 })
-// //4 Використовуйте observer для спостереження за всіма елементами li:
+
+
 fruitItems.forEach((item) => {
-  observerParameters.observe(item)
+  observerCallback.observe(item);
 })
-
-
-
-
 
 
 
