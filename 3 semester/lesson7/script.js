744934074dca390fccdcce9ff3da932137725cc6
Olha36 <olhakucheruk36@gmail.com>
2024-03-08 18:37:20 +0200

    add

--- 3 semester/lesson7/script.js
+++ 3 semester/lesson7/script.js
@@ -1,52 +1,67 @@
-// const arr = ['1,', '2', 'true', ' kiwi'];
-// arr[3] = 'pineapple';
+//example 1
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(matrix[1][1]);
+// let total = 0;
 
-// // console.log(arr[3]);
+// for (let i = 0; i < matrix.length; i += 1) {
+//   console.log('Підмасив матриці matrix[i]: ', matrix[i]);
 
-// arr.push('strawberry');
-
-// for (let i = 0; i < arr.length; i++) {
-//   // console.log(arr[i]);
-//   if (arr[i] === arr[3]) {
-//     continue;
-   
+//   for (let j = 0; j < matrix[i].length; j += 1) {
+//     console.log('Елемент підмасив матриці matrix[i][j]: ', matrix[i][j]);
+//     total += matrix[i][j];
 //   }
-//   // console.log(arr[i]);
-  
+// } 
+// console.log(total);
+
+// const number = ['ant', 'kiwi', 'banana', 'apple', 'english'];
+// const split = number.push('goiteens')
+// const unshiftMethod = number.unshift('Ukraine')
+// // const shiftMethod  = number.shift();
+// // const sliceMethod = number.slice(2, 5);
+// const popMethod = number.pop()
+// console.log(split);
+// console.log(number);
+// // console.log(sliceMethod);
+
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 2, 'July');
+// // Inserts at index 1
+// console.log(months);
+
+// const concatMethod = number.concat(months);
+// console.log(concatMethod);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 9];
+
+// // console.log(arr[2]);
+
+// // console.log(arr.indexOf(5));
+
+// let i = 0;
+// while (arr.length > i) {
+//   console.log(arr[i]);
+//   console.log(arr);
+//   i++;
 // }
 
-// for (item of arr) {
-//   console.log(item);
+
+// for(let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (i of arr) {
+//   console.log([i]);
+//   console.log(arr[i]);
 // }
 
 
-// let multipleArr = [
-//   ['1', '2', '3', '4'],
-//   ['katya', 'masha', 'vasya', 'peter'],
-//   ['kiwi', 'apple', 'orange', 'tangerin']
-// ]
-// console.log(multipleArr[2][1]);
+// let arr1 = [1, 5,'4', 'hello'];
+// let arr2 = [true,2, {}, ['a'], 222];
 
-// for (let i = 0; i < multipleArr.length; i++) {
-//   console.log(multipleArr[i]);
-//   for (let j = 0; j < multipleArr.length; j++) {
-//     console.log(multipleArr[j]);
-
-    
-//   }
-// }     
-
-// const arr1 = [1, 5, '4', 'hello'];
-// const arr2 = ['true', 2, '{}', ['a'], 222];
-// console.log(`this is arr1: ${arr1}`);
-// console.log(`this is arr2: ${arr2}`);
+// console.log(arr1.indexOf(5));
+// console.log(arr2.indexOf(2));
 // console.log(arr1[1] + arr2[1]);
-
-const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
-let total = 0;
-
-for (let i = 0; i <= cart.length; i++) {
-  // console.log(cart[i]);
- total += cart[i];
- console.log(total);
-}
