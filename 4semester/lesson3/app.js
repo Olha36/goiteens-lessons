91bcd2a64487e8c22b54ea9811f49ff348205b62
Olha36 <olhakucheruk36@gmail.com>
2024-03-09 14:05:13 +0200

    finish lesson3

---
+++ 4semester/lesson3/app.js
@@ -0,0 +1,39 @@
+var validator = require('validator');
+var prompt = require('prompt');
+
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['email'], (err, result) => {
+
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let email = result.email;
+
+  if(validator.isEmail(email)) {
+    console.log('email is valid');
+  } else {
+    console.log('email is not valid');
+  }
+});
+
+// const validateEmail = email => {
+//   return validator.isEmail(email);
+// }
+
+// console.log("Is mango@mail.com a valid email?:",
+// validateEmail('mango@mail.com')
+// );
+
+// console.log("Is mangomail.com a valid email?:",
+// validateEmail('mangomail.com')
+// );
+
+
+
+// console.log('hello');
+
