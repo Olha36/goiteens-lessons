6c0707a48d0d8e628e2333c6245422095fb1c512
Olha36 <olhakucheruk36@gmail.com>
2024-03-16 20:15:29 +0200

    add hometasks

--- hometask/script.js
+++ hometask/script.js
@@ -1,10 +1,62 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr = [1, 2, 3, 4, 65, 6, 7, 8, 9, 10, 11, 12, 13];
+// let sum = 0;
 
-const cards = [
-  'card-1',
-  'card-2',
-  'card-3',
-  'card-4',
-  'card-5',
-]
+
+// for(let i = 0; i <= arr.length; i++) {
+//   // console.log(arr[i]);
+//   sum += arr[i]; // sum = arr[i] + sum
+//   console.log(sum);
+// }
+// console.log(sum);
+
+
+let max = Math.max(...arr);
+console.log(max);
+
+
+// let num = 0;
+
+// while(num <= arr.length) {
+//   console.log(arr[num]);
+//   num++;
+// }
+
+// for(let i = 0; i <= arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// console.log(typeof arr);
+
+// let name = 'Danya';
+// console.log(typeof name);
+
+// let age = 13;
+// console.log(typeof age);
+
+// let boolean = true;
+// console.log(typeof boolean)
+
+// const cards = [
+//   'card-1',
+//   'card-2',
+//   'card-3',
+//   'card-4',
+//   'card-5',
+// ]
+
+// let num = 1;
+
+// while(num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// for(let i = 1; i <= 20; i++) {
+//   // console.log(i);
+//   if(i % 2) {
+//     console.log(i);
+//     continue;
+//   }
+  
+// }
 
