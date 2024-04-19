4e98855032a285e9657db25aa021751e52d87f88
Olha36 <olhakucheruk36@gmail.com>
2024-04-19 18:11:31 +0300

    lesson 18.04

--- 3 semester/lesson19/script.js
+++ 3 semester/lesson19/script.js
@@ -1,5 +1,56 @@
-const header = document.querySelector('.main-header');
+// const h1 = document.querySelector('.main-header');
+// console.log(h1);
 
-header.textContent = 'It is a main header';
+// h1.textContent = 'I love my students!!!';
 
-header.classList.add('main-header');
+// h1.style.color = 'red'
+
+// h1.classList.add('bgc')
+
+// const siteNavUl = document.querySelector('.site-nav');
+// console.log(siteNavUl);
+
+// const li = document.querySelectorAll('.site-nav__item');
+// console.log(li);
+
+// ['site-nav__item', 'site-nav__item', 'site-nav__item']
+
+// li.style.backgroundColor = 'orange';
+// li.forEach((item) => {
+//   item.style.backgroundColor = 'orange';
+// })
+
+// const ulList = document.querySelector('.site-nav');
+// console.log(ulList);
+
+// ulList.style.backgroundColor = 'yellow';
+
+// const li = document.querySelectorAll('a');
+// console.log(li);
+// console.log(li[0]);
+
+// const firstChildElement = li[0];
+// console.log(li[2]);
+
+// const lastChildElement = li[2];
+// firstChildElement.classList.add('navigation');
+// lastChildElement.classList.add('lastElement');
+
+// const secondElement = li[1];
+// console.log(secondElement);
+
+// secondElement.style.fontSize = '50px'
+
+// const a = document.querySelectorAll('ul a'); //['a', 'a', 'a']
+// console.log(a);
+
+// a.forEach((link) => link.style.color = 'orange')
+
+const heading1 = document.querySelector('h1');
+console.log(heading1);
+
+heading1.textContent = 'Thanks God!'
+
+heading1.classList.add('styles')
+
+console.log(heading1.classList.contains('list'))
