3c60f7a24f3d6bcd78e9a523bb2c6c4c92c695dd
Olha36 <olhakucheruk36@gmail.com>
2024-04-23 18:36:59 +0300

    lesson 23.04

--- 4semester/lesson11/index.js
+++ 4semester/lesson11/index.js
@@ -16,37 +16,47 @@
 // const start = document.querySelector('.button-start');
 // const stop = document.querySelector('.button-stop');
 // const reset = document.querySelector('.button-reset');
+// const text = document.querySelector('.seconds');
+
 
 // let startTime;
 // let timerInterval;
+// let running = false;
+// let pausedTime = 0;
 
 
 // function interval () {
 //   timerInterval = setInterval(() => {
 //     const currentTime = new Date();
-//     const elapsedTime = currentTime - startTime;
-//     const hours = Math.floor(elapsedTime / 3600000)
-//     // const hours = currentTime.getHours();
-//     // const minutes = currentTime.getMinutes();
-//     // const seconds = currentTime.getSeconds();
-//     // const miliSeconds = currentTime.getMilliseconds();
-//     // console.log(`${hours}:${minutes}:${seconds}.${miliSeconds}`);
-//     console.log(hours);
-//   }, 1000)
+//     const elapsedTime = currentTime - startTime + pausedTime;
+//     const hours = Math.floor(elapsedTime / 3600000);
+//     const minutes = Math.floor((elapsedTime % 3600000)/ 60000);
+//     const seconds = Math.floor((elapsedTime % 60000)/ 1000);
+// 		const miliSeconds = elapsedTime % 1000;
+// 	}, 1000)
 // }
-// interval()
-
 
 // const handleStart =() => {
-// interval();
+// 	if(!running) {
+// 		startTime  = new Date();
+// 		interval();
+// 		running = true;
+// 	}
+	
 // }
 
 // const handleStop = () => {
-//   clearInterval(timerInterval)
+// 	if(running) {
+// 		clearInterval(timerInterval);
+// 		pausedTime += new Date() - startTime;
+// 		running = false;
+// 	}
 // }
 
 // const handleReset = () => {
-//   clearInterval(timerInterval)
+//   clearInterval(timerInterval);
+// 	running = false;
+// 	pausedTime = 0;
 // }
 
 // start.addEventListener('click', handleStart);
@@ -54,26 +64,35 @@
 // reset.addEventListener('click', handleReset);
 
 
-let timeLeft = 30000;
-const timerDisplay = document.querySelector('.displayTimer');
-const startButton = document.querySelector('.startTimer');
+//task 3
+// const background = document.querySelector('body');
 
-const startTimer = () => {
-	timeLeft = 30000;
-	startButton.disabled = true;
-	let timer = setInterval(() => {
-		timeLeft -= 10;
-		timerDisplay.textContent = timeLeft / 1000;
+// let timer = 11;
 
-		if (timeLeft <= 10000) {
-			timerDisplay.style.color = 'red';
-		} else {
-			timerDisplay.style.color = 'black';
-		}
-		if (timeLeft === 0) {
-			clearInterval(timer);
-			startButton.disabled = false;
-			timerDisplay.textContent = 'Час закінчився!';
-		}
-	}, 10);
-};
+// const interval = setInterval(() => {
+// 	timer--;
+
+// 	if(timer === 5) {
+// 		background.style.backgroundColor = 'red';
+// 	} 
+
+// 	if(timer === 0) {
+// 		clearInterval(interval)
+// 	}
+// 	console.log(timer);
+// }, 1000)
+
+
+//task 4
+function clock() {
+	const currentTime = new Date();
+	const hours = currentTime.getHours();
+	const minutes = currentTime.getMinutes();
+	const seconds = currentTime.getSeconds();
+
+	return `${hours}:${minutes}:${seconds}`
+}
+
+setInterval(() => {
+	console.log(clock());
+}, 1000)
