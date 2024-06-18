d9d1a7c8d23758ee992843425fa69a889cdb6f27
Olha36 <olhakucheruk36@gmail.com>
2024-06-18 21:38:09 +0300

    lesson 18.06

--- 4semester/lesson1/script.js
+++ 4semester/lesson1/script.js
@@ -38,48 +38,48 @@
 // parent.addEventListener('click', example2)
 
 // task2
-const colorPalette = document.querySelector(".color-palette");
-const output = document.querySelector(".output");
+// const colorPalette = document.querySelector(".color-palette");
+// const output = document.querySelector(".output");
 
-// This is where delegation «magic» happens
-function selectColor(event) {
-  if (event.target.nodeName !== "BUTTON") {
-    return;
-  }
+// // This is where delegation «magic» happens
+// function selectColor(event) {
+//   if (event.target.nodeName !== "BUTTON") {
+//     return;
+//   }
 
-  const selectedColor = event.target.dataset.color; // на клікнутий елемент встановити еолір і це все помістити в змінну selectedColor
-  output.textContent = `Selected color: ${selectedColor}`;
-  output.style.color = selectedColor;
-}
+//   const selectedColor = event.target.dataset.color; // на клікнутий елемент встановити еолір і це все помістити в змінну selectedColor
+//   output.textContent = `Selected color: ${selectedColor}`;
+//   output.style.color = selectedColor;
+// }
 
-// Some helper functions to render palette items
-createPaletteItems();
+// // Some helper functions to render palette items
+// createPaletteItems();
 
-function createPaletteItems() {
-  const items = [];
-  for (let i = 0; i < 60; i++) {
-    const color = getRandomColor();
-    const item = document.createElement("button");
-    item.type = "button";
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add("item");
-    items.push(item);
-  }
-  colorPalette.append(...items);
-}
+// function createPaletteItems() {
+//   const items = [];
+//   for (let i = 0; i < 60; i++) {
+//     const color = getRandomColor();
+//     const item = document.createElement("button");
+//     item.type = "button";
+//     item.dataset.color = color;
+//     item.style.backgroundColor = color;
+//     item.classList.add("item");
+//     items.push(item);
+//   }
+//   colorPalette.append(...items);
+// }
 
-function getRandomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-}
+// function getRandomColor() {
+//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+// }
 
-function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-}
+// function getRandomHex() {
+//   return Math.round(Math.random() * 256)
+//     .toString(16)
+//     .padStart(2, "0");
+// }
 
-colorPalette.addEventListener("click", selectColor);
+// colorPalette.addEventListener("click", selectColor);
 
 //example1
 // const btns = document.querySelectorAll('.btn');
@@ -97,11 +97,35 @@ colorPalette.addEventListener("click", selectColor);
 // const button = document.querySelectorAll('.deleteButton')
 
 // function deleteUser(event) {
-
+//   // console.log(event.target);
+//   if(event.target.classList.contains('deleteButton')) {
+//     console.log('button');
+//     const child = event.target.closest('li');
+//     console.log(child);
+//     child.remove();
+//   }
 // }
+
+// button.forEach((btn) => {
+//   btn.addEventListener('click', deleteUser);
+
+// })
 
 //task4
 const buttonList = document.querySelectorAll(".btn");
 console.log(buttonList);
 
-const hightLightElement = (event) => {};
+const hightLightElement = (event) => {
+  console.log(event.target);
+
+   buttonList.forEach((btn) => {
+    btn.classList.remove('active')
+  })
+
+  event.target.classList.add('active')
+};
+
+buttonList.forEach((btn) => {
+  btn.addEventListener('click', hightLightElement);
+})
+
--- 4semester/lesson2/script.js
+++ 4semester/lesson2/script.js
@@ -1,42 +1,47 @@
 const vanillaOutput = document.querySelector(".output.vanilla");
 const throttledOutput = document.querySelector(".output.throttled");
 const debouncedOutput = document.querySelector(".output.debounced");
-const eventCounter = {
-  vanilla: 0,
-  throttled: 0,
-  debounced: 0,
-};
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+//   debounced: 0,
+// };
 
 
-document.addEventListener("scroll", () => {
-  eventCounter.vanilla += 1;
-  vanillaOutput.textContent = eventCounter.vanilla;
-});
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
 
 
-document.addEventListener(
-  "scroll",
-  _.throttle(() => {
-    eventCounter.throttled += 1;
-    throttledOutput.textContent = eventCounter.throttled;
-  }, 300)
-);
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
 
-document.addEventListener(
-  "scroll",
-  _.debounce(() => {
-    eventCounter.debounced += 1;
-    debouncedOutput.textContent = eventCounter.debounced;
-  }, 300)
-)
-
-
-_.forEach([1, 2, 5, 8, 10, 15], function(value) {
-  console.log(value);
-});
-
-console.log(_.includes([1, 2, 3, 4], 2, 1));
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     eventCounter.debounced += 1;
+//     debouncedOutput.textContent = eventCounter.debounced;
+//   }, 300)
+// )
 
 
+// _.forEach([1, 2, 5, 8, 10, 15], function(value) {
+//   console.log(value);
+// });
+
+// console.log(_.includes([1, 2, 3, 4], 2, 1));
+
+function updatePosition() {
+  console.log('Hellow world!!!');
+}
+
+
+document.addEventListener('scroll', _.throttle(updatePosition, 6000));
 
 
