7a366675f8fb57ac98f71011e3879f575ce2f056
Olha36 <olhakucheruk36@gmail.com>
2024-06-27 20:05:04 +0300

    lesson 27.06 intersection observer

--- 4semester/intersectionObserver/script.js
+++ 4semester/intersectionObserver/script.js
@@ -15,22 +15,27 @@
 //task 1
 const fruitsList = document.querySelectorAll('.fruits-list li');
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const observer = new IntersectionObserver((entries, observer) => {
+  const observer = new IntersectionObserver((entries) => {
     // console.log(entries);
     entries.forEach(entry => {
+      const imgTitle = entry.target.querySelector('h3');
+      const image = entry.target.querySelector('img');
+      
       if(entry.isIntersecting) {
-        console.log('hi', entry);
+        const newImg = image.getAttribute('data-source')
+        imgTitle.style.color = 'blue';
+        image.style.transform = 'scale(1.5)';
+        image.src = newImg;
+        observer.unobserve(entry.target)
+      }  else {
+        imgTitle.style.color = 'red';
       }
     })
     
-  })
+  }, {threshold: 0.5})
 
   fruitsList.forEach(fruit => {
    observer.observe(fruit)
   })
 
 
-})
