e8f47a5c143f6818a6147140dd2180536e6b55e4
Olha36 <olhakucheruk36@gmail.com>
2024-07-13 16:42:09 +0300

    prepare files for a crud ht

---
+++ 4semester/lesson17crud/task2.js
@@ -0,0 +1,16 @@
+import * as dataMovies from './db.json' assert { type: 'json' }
+console.log(dataMovies.default.movies);
+
+function getMoviesFromDatabase() {
+
+}
+
+function addMoviesToDatabase() {
+
+}
+
+function updateMovies() {}
+
+function updateMovieById() {}
+
+function addMovieById() {}
