132b6a2d153063037a103d8496231e2f0c9b850a
Olha36 <olhakucheruk36@gmail.com>
2024-07-20 21:11:07 +0200

    create handlebars part

---
+++ dist/index.672d4772.js
@@ -0,0 +1,16 @@
+// const menuData = {
+//   title: 'Eat it createElement, templates rule!',
+//   items: ['Handlebars', 'LoDash', 'Pug', 'EJS', 'lit-html'],
+// };
+// const employees  = {
+//   name: 'person1',
+//   age: 35,
+//   position: 'teacher'
+// }
+// console.log('check');
+// const source = document.querySelector('#menu-template').innerHTML; //в меню темплейт буде додано
+// const template = Handlebars.compile(source); // скомпілювали і додали
+// document.querySelector('#menu-container').innerHTML = template(employees)
+console.log("ccheck");
+
+//# sourceMappingURL=index.672d4772.js.map
