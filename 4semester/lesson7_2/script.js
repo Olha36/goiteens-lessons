864bf5c4de8a868e4928ecf2d5d51869e4878bc5
Olha36 <olhakucheruk36@gmail.com>
2024-07-24 17:49:33 +0200

    lesson 23.07

--- 4semester/lesson7_2/script.js
+++ 4semester/lesson7_2/script.js
@@ -1,8 +1,40 @@
-const userData = {
-    name: "John Rolph",
-    email: "john@example.com",
-    age: 40,
-    greetings: "hello"
-};
-const userTemplate = Handlebars.compile(document.querySelector('#address-template').innerHTML);
-document.querySelector('#user-template').innerHTML = userTemplate(userData);
+const userData = [
+    {
+        name: "John Rolph",
+        email: "john@example.com",
+        age: 25,
+    },
+
+    {
+        name: "Oleh",
+        email: "oleh@example.com",
+        age: 13,
+    },
+
+    {
+        name: "Edward",
+        email: "edward@example.com",
+        age: 13,
+    },
+
+    {
+        name: "Danya",
+        email: "danya@example.com",
+        age: 13,
+    },
+
+    {
+        name: "Yura",
+        email: "yura@example.com",
+        age: 13,
+    },
+
+    {
+        name: "Nazar",
+        email: "nazar@example.com",
+        age: 14
+    }
+];
+let template = Handlebars.compile(document.querySelector('#address-template').innerHTML);
+// execute the compiled template and print the ou
+document.querySelector('#user-template').innerHTML = template(userData)
