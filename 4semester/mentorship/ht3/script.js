ca698271710e70cba4503bc2c1ff5287c4cc42ee
Olha36 <olhakucheruk36@gmail.com>
2024-08-03 19:05:06 +0200

    mentorship

--- 4semester/mentorship/ht3/script.js
+++ 4semester/mentorship/ht3/script.js
@@ -1,54 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
   const images = document.querySelectorAll('img[data-src]');
-  // console.log(images);
-  const loadButton = document.querySelector('#loadButton');
-  // console.log(loadButton);
+  console.log(images);
+
+  const imgOrg = [];
 
   const options = {
-    root: null,
-    rootMargin: '0px',
     threshold: 0.5
   }
-
-  const originalSrcs = [];
-
-  const loadImage = function(entry) {
+ 
+  function loadImage(entry) {
     const image = entry.target;
-    console.log(image);
-    const originalSrc = image.getAttribute('src');
-    originalSrcs.push(originalSrc);
+
+    const originalImage = image.getAttribute('src');
+    console.log(originalImage);
+    
+    imgOrg.push(originalImage);
+   
     image.src = image.getAttribute('data-src');
-    image.classList.remove('lazy-load');
-    observer.unobserve(image);
+
+    observer.unobserve(image)
+
   }
 
-  const revertImages = function() {
-    images.forEach((image, index) => {
-      const dataSrc = image.getAttribute('data-src');
-      const originalSrc = originalSrcs[index];
-      image.src = originalSrc;
-      image.setAttribute('data-src', originalSrc);
-      originalSrcs[index] = dataSrc;
-      image.classList.add('lazy-load')
-      observer.observe(image)
-    })
-    originalSrcs.length = 0;
-  }
 
   const observer = new IntersectionObserver(function(entries, observer) {
     entries.forEach(entry => {
       if(entry.isIntersecting) {
-        loadImage(entry)
+        loadImage(entry);
       }
-    });
-    } , options)
-
-    images.forEach(image => {
-      observer.observe(image)
     })
-
-    loadButton.addEventListener('click', function() {
-      revertImages()
-    })
+    
+  }, options)
+  
+  images.forEach(image => {
+    observer.observe(image)
   })
+ 
+})
 
