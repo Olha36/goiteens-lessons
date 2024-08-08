f6ac4ff513efb9c0f86447816dd5d04b49c192b0
Olha36 <olhakucheruk36@gmail.com>
2024-08-08 10:44:43 +0200

    addmore examples for children

--- 4semester/lesson9/examples.js
+++ 4semester/lesson9/examples.js
@@ -1,25 +1,27 @@
 // example 1
-// const settings = {
-//   theme: 'dark',
-//   isAuthenticated: true,
-//   options: [1, 2, 3],
-// };
-// // console.log(settings);
+const settings = {
+  theme: 'dark',
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+// console.log(settings);
 
-// localStorage.setItem('settings', JSON.stringify(settings));
+// localStorage.setItem('settings', JSON.stringify(settings))
 
 // const storedSettings = localStorage.getItem('settings');
+// console.log(storedSettings);
+
 // const parsedSettings = JSON.parse(storedSettings);
 
 // console.log(parsedSettings);
 
 
 //example 2
-// const input = document.querySelector('.example2');
-// const button = document.querySelector('.button');
+const input = document.querySelector('.example2');
+const button = document.querySelector('.button');
 
-// button.addEventListener('click', function() {
-//   localStorage.info = input.value;
-// })
+button.addEventListener('click', function() {
+  localStorage.info = input.value;
+})
 
-// document.write(localStorage.info)
+document.write(localStorage.info)
--- 4semester/lesson9/script.js
+++ 4semester/lesson9/script.js
@@ -1,38 +1,44 @@
-// task 1
-function addToLocalStorage(key, object) {
-  let objectString = JSON.stringify(object);
+// function addObject(key, object) {
+//  const objectJson = JSON.stringify(object);
 
-  localStorage.setItem(key, objectString)
-  return object;
-}
+//  localStorage.setItem(key, objectJson)
+// }
 
-const key = 'task 1'
+// const object = {
+//   fly: "hippo",
+//   sit: "plane"
+// }
 
-const object = {
-  name: 'John',
-  age: 30
-}
+// let key = 'Let/s add info to localstorage'
 
-addToLocalStorage(key, object);
-
+// addObject(key, object)
 
 
 //task 2
-function getFromLocalStorage(key) {
- const stringToObj = localStorage.getItem(key);
+// function getItem(key) {
+//   console.log(key);
+  
+//  const getItemFromStorage = localStorage.getItem(key);
+// console.log(getItemFromStorage);
 
-  const object = JSON.parse(stringToObj);
-  return object;
-}
-
-// console.log(getFromLocalStorage(key));
-//task 3
-
-// function removeItem() {
-//   localStorage.removeItem(key)
+//   const objectJson = JSON.parse(getItemFromStorage);
+//   console.log(objectJson) ;
+  
 // }
+// getItem(key);
 
-// removeItem(key)
+// task 3
+// function removeKeys(key) {
+//   console.log(key);
+  
+//  localStorage.removeItem(key);
+
+
+//  console.log(object);
+ 
+// }
+// removeKeys(key)
+
 // task 4
 // function clearStorage() {
 //   localStorage.clear();
@@ -40,9 +46,9 @@ function getFromLocalStorage(key) {
 // clearStorage();
 
 //task 5
-function lengthStorage() {
-  return localStorage.length;
-}
+// function lengthStorage() {
+//   return localStorage.length;
+// }
 // console.log(lengthStorage());
 
 //task 6
@@ -61,59 +67,59 @@ function lengthStorage() {
 // localStorage.setItem('key1', 'value1');
 // localStorage.setItem('key2', 'value2');
 
-function  returnList() {
-  const keys = [];
+// function  returnList() {
+//   const keys = [];
   
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i)
-    if (key !== null) {
-      keys.push(key);
-    }
-  }
-  return keys;
+//   for (let i = 0; i < localStorage.length; i++) {
+//     const key = localStorage.key(i)
+//     if (key !== null) {
+//       keys.push(key);
+//     }
+//   }
+//   return keys;
   
-  }
+//   }
 //   console.log(returnList());
 //task 7
-function checkKey(key) {
-  return localStorage.getItem(key) !== null;
-}
+// function checkKey(key) {
+//   return localStorage.getItem(key) !== null;
+// }
 // console.log(checkKey());
 //task 8
-function checkValue(value) {
-  return localStorage.getItem(value) !== null;
-}
+// function checkValue(value) {
+//   return localStorage.getItem(value) !== null;
+// }
 // console.log(checkValue('somevalue1'));
 
 //task 9
-function addItems(item, value) {
-  const existingArr = JSON.parse(localStorage.getItem(item)) || [];
+// function addItems(item, value) {
+//   const existingArr = JSON.parse(localStorage.getItem(item)) || [];
 
-  existingArr.push(value);
+//   existingArr.push(value);
 
-  localStorage.setItem(item, JSON.stringify(existingArr))
-}
-addItems('myArray', 'value45');
-console.log(JSON.parse(localStorage.getItem('myArray')));
+//   localStorage.setItem(item, JSON.stringify(existingArr))
+// }
+// addItems('myArray', 'value45');
+// console.log(JSON.parse(localStorage.getItem('myArray')));
 
 
 
 //3
-const name = prompt('Enter your name');
-const age = prompt('Enter your age');
-const email = prompt('Enter your email');
 
-// 3.2
 
-function handleSubmit(event) {
-  event.preventDefault();
+function handleSubmit() {
+  const name = prompt('Enter your name');
+  const age = prompt('Enter your age');
+  const email = prompt('Enter your email');
+  
+  const info = {
+    name: name,
+    age: age,
+    email: email
+  }
+  console.log(info);
 
-  let name = document.getElementById('name').value;
-  console.log(name);
-  let age = document.getElementById('age').value;
-  console.log(age);
-  let email = document.getElementById('email').value;
-  console.log(email);
+  localStorage.setItem('userInfo', JSON.stringify(info));
 }
-
+handleSubmit();
  
