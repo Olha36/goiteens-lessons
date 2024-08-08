2c1302c95d37e44f2f998d476b1a239dc6c8ee2a
Olha36 <olhakucheruk36@gmail.com>
2024-08-08 11:02:09 +0200

    install react with vite

---
+++ semester5/lesson2-components/my-react-app/vite-installation-commands.md
@@ -0,0 +1,5 @@
+1. npm create vite@latest
+2. choose a framework
+3. cd vite-project
+4. npm install
+5. npm run dev
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>МОДУЛЬ №2 Компоненти</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
