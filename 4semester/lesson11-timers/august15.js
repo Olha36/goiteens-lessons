52e18ecfa083807ef8a6727bc9fec045e000d4ee
Olha36 <olhakucheruk36@gmail.com>
2024-08-21 17:58:16 +0200

    lesson 20.08

--- 4semester/lesson11-timers/august15.js
+++ 4semester/lesson11-timers/august15.js
@@ -2,19 +2,19 @@
 
 // setTimeout(() => {
 //     console.log('settime out 1 try');
-    
+
 // }, 1000);
 
 // console.log('Hello, 2');
 
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
 // let timer = 0;
 
 // let interval = setInterval(() => {
 //    timer++;
 //    console.log(timer);
-   
+
 //    if(timer >= 20) {
 //     clearInterval(interval);
 //    }
@@ -26,7 +26,7 @@ const body = document.querySelector('body');
 //    } else {
 //     body.style.backgroundColor = 'yellow'
 //    }
-    
+
 // }, 1000);
 
 // task 2
@@ -39,25 +39,40 @@ const body = document.querySelector('body');
 //     if(interval === 5) {
 //         body.style.backgroundColor = 'red';
 //     }
-    
+
 //     else if(interval === 0) {
 //         clearInterval(intervalAdd)
 //     }
 // }, 1000)
 
 // 2:0:0:0
-// 180
+// 123 2 3.   2 hours 3 minutes
 const startTimerButton = document.querySelector('.button-start');
 const stopTimerButton = document.querySelector('.button-stop');
 const resetTimerButton = document.querySelector('.button-reset');
 
 let timer = 0;
+let elapsedTime = 0;
 
-function countTime(elapsedTime) {
-    const hours = String(Math.floor(elapsedTime / 360000)).padStart(2, '0')
-    // const minutes
-    // const seconds
-    // const miliseconds
+function countTime() {
+//   const totalSeconds = Math.floor(elapsedTime / 100);
+  const hours = Math.floor(elapsedTime / 3600000);
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor(elapsedTime / 1000);
+  const miliseconds = Math.floor(elapsedTime / 1000)
 
-    return `${hours}:${minutes}:${seconds}:${miliseconds}`
+//   return `${hours}:${minutes}:${seconds}:${miliseconds}`;
+  console.log(`${hours}:${minutes}:${seconds}:${miliseconds}`);
 }
+countTime();
+
+function startTimer() {
+    let startTime = Date.now() - elapsedTime;
+    console.log(startTime);
+
+    setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+    })
+    
+}
+startTimer()
