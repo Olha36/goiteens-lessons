bd3ece279e76f66dd840e6ed6ae29e477cc9a12d
Olha36 <olhakucheruk36@gmail.com>
2024-09-10 18:57:21 +0200

    prepare lessson6

---
+++ semester5/lesson6-practice-css-in-js/goiteens-events-js/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const PageBoard = ({ name, location, speaker, type, start, end }) => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <PageBoard />
+    </>
+  );
+}
+
+export default App;
