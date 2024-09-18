79388500cd6e1f704428d59b8d6616bef0ecfb96
Olha36 <olhakucheruk36@gmail.com>
2024-09-18 19:51:16 +0200

    finish addTask function

--- semester5/lesson7-components-classes-events/hometask/src/App.jsx
+++ semester5/lesson7-components-classes-events/hometask/src/App.jsx
@@ -10,20 +10,34 @@ class TaskList extends Component {
     };
   }
 
+  componentDidMount() {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (savedTasks) {
+      this.setState({ tasks: savedTasks });
+    }
+  }
+
   // function to get input value
   handleInputChange = (e) => {
     this.setState({ newTask: e.target.value });
-    console.log((e.target.value));
-    
+    console.log(e.target.value);
   };
 
-  // function to dd a task
+  // function to add a task
 
   addTask = () => {
-    const {task, newTask} = this.setState;
-    
-    
-  }
+    // створити змінні для зберігання тексту нового завдання та масиву списку всіх завдань
+    const { tasks, newTask } = this.state;
+    // Перед тим, як додавати завдання, треба впевнитись, що воно не порожнє.
+    if (newTask) {
+      const updatedTasks = [...tasks, newTask];
+      this.setState({
+        tasks: updatedTasks,
+        newTask: '',
+      });
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    }
+  };
 
   render() {
     return (
@@ -35,11 +49,17 @@ class TaskList extends Component {
           placeholder='Додайте нове завдання'
           onChange={this.handleInputChange}
         />
-        <button className='add-task' onClick={this.addTask}>Додати</button>
+        <button className='add-task' onClick={this.addTask}>
+          Додати
+        </button>
         <ul className='tasks'>
-          <li></li>
-          <button className='delete-task'>Видалити</button>
-          <button className='edit-task'>Редагувати</button>
+          {this.state.tasks.map((text, id) => (
+            <li key={id}>
+              {text}
+              <button className='delete-task'>Видалити</button>
+              <button className='edit-task'>Редагувати</button>
+            </li>
+          ))}
         </ul>
       </div>
     );
