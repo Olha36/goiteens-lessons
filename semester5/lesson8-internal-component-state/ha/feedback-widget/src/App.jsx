dffcad790c6fc7232a4b54a6d8615a7a028687c3
Olha36 <olhakucheruk36@gmail.com>
2024-09-21 20:09:37 +0200

    finish step 1 from hometask

---
+++ semester5/lesson8-internal-component-state/ha/feedback-widget/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleGoodFeedback = () => {
+    setState(prevState => ({...prevState, good: prevState.good + 1}))
+  }
+
+  const handleNeutralFeedback = () => {
+    setState(prevState => ({...prevState, neutral: prevState.neutral + 1}))
+  }
+
+  const handleNegativeFeedback = () => {
+    setState(prevState => ({...prevState, bad: prevState.bad + 1}))
+  }
+
+
+  return (
+    <div className='feedback-container'>
+      <h1>Please leave feedback</h1>
+
+      <div className='button-wrapper'>
+        <button className='feedback-positive' onClick={handleGoodFeedback}>Good</button>
+        <button className='feedback-neutral' onClick={handleNeutralFeedback}>Neutral</button>
+        <button className='feedback-negative' onClick={handleNegativeFeedback}>Bad</button>
+      </div>
+
+      <h2>Statistics</h2>
+      <p>Good: {state.good}</p>
+      <p>Neutral: {state.neutral}</p>
+      <p>Bad: {state.bad}</p>
+    </div>
+  );
+}
+
+export default App;
