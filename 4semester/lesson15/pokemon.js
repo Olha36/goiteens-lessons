6ed4fcecafaa30158a402b5a425799ccd1739464
Olha36 <olhakucheruk36@gmail.com>
2024-10-03 14:38:08 +0200

    lesson 01.10

--- 4semester/lesson15/pokemon.js
+++ 4semester/lesson15/pokemon.js
@@ -1,19 +1,43 @@
-const form = document.getElementById('search-form');
-const pokeId = document.getElementById('pokemon-id');
-const button = document.getElementById('search-button');
-
-
-async function getDateAPi(name) {
-  const pokeApi = 'https://pokeapi.co/api/v2/pokemon/';
-  const responseApi = await fetch(pokeApi);
-  if (!responseApi.ok) {
-    throw new Error(`could not fetch DATA for ${name}:responseApi.statusText`);
-  }
+const form = document.getElementById("search-form"); 
+const inputPokeId = document.getElementById("pokemon-id"); 
+const button = document.getElementById("search-button"); 
+const container = document.getElementById("pokemon-container"); 
+ 
+form.addEventListener('submit', getPokemonFetchData) 
+ 
+function getPokemonFetchData(event) { 
+  console.log('hi'); 
+  event.preventDefault() //Запобігає автоматичному оновленню сторінки 
+  const value = inputPokeId.value //отримуємо значення інпуту 
+  console.log(value) 
+  if(value) { //Якщо в інпуті введено значення  
+    try { 
+const getDataApiSave = getDataApi(value) 
+inputPokeId.value = '' 
+    } catch(error) { 
+      errors(error.message)
+    } 
+  } 
+} 
+getPokemonFetchData() 
+ 
+async function getDataApi(name) { 
+  const pokeApi = 'https://pokeapi.co/api/v2/pokemon/'; 
+  const responseApi = await fetch(pokeApi) 
+  if (!responseApi.ok) { 
+    throw new Error(`Could not fetch data for ${name}:responseApi.statusText`); 
+  } 
+  return responseApi.json()  
+} 
+ 
+function displayPokemonDate(pokemon) { 
+  const layout = ` 
+    <div> 
+        <p>name: ${pokemon.name}</p> 
+        <p>height: ${pokemon.height}</p> 
+        <p>abilities: ${pokemon.ability.name}</p> 
+        <p>type: ${pokemon.type}</p> 
+    </div> 
+    `; 
+  container.innerHTML = layout; 
 }
-getDateAPi();
-// обробник події на форму має містити функцію яка відповідаж за знаходження елементів:
-// - знайти значення інпута та врахувати кейс. Всі літери автоматично трансформувати в малі
-// якщо дані знайдені... виводимо картки або помилку
-// фунеція для відображення даних в якій буде розмітка
-// функція для виводу помилок
-
