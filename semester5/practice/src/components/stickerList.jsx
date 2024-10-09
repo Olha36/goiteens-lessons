33d33ee43d9e426f18064dede1eb4546f5edd023
Olha36 <olhakucheruk36@gmail.com>
2024-10-09 18:12:45 +0200

    add styled component

--- semester5/practice/src/components/stickerList.jsx
+++ semester5/practice/src/components/stickerList.jsx
@@ -1,20 +1,27 @@
 import { PropTypes } from 'prop-types';
 import stickers from '../assets/stickers.json';
 import Sticker from '../components/sticker';
+import styled from 'styled-components';
 
 StickerList.propTypes = {
   onStickerSelect: PropTypes.func,
 };
 
+const  StickersContainer = styled.h1`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
 // a component that will render all stickers from the file stickers.json
 export default function StickerList({ onStickerSelect }) {
   return (
     <div>
-      <div className='stickers-container'>
+      <StickersContainer>
         {stickers.map((sticker, index) => (
           <Sticker key={index} img={sticker.img} label={sticker.label} onClick={onStickerSelect} />
         ))}
-      </div>
+      </StickersContainer>
     </div>
   );
 }
