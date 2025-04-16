60cfbbe21bc8b04e496cfc69adc203ba0137ee1e
olha36 <olhakucheruk36@gmail.com>
2025-04-16 20:20:25 +0300

    lesson 2 react

---
+++ semester5/lesson1-react-library/lesson1-practice/src/Dania.jsx
@@ -0,0 +1,9 @@
+function Dania() {
+    return (
+      <div>
+        <h2>Another component</h2>
+       <p>Dania</p>
+      </div>
+    );
+  }
+  export default Dania;
---
+++ semester5/lesson1-react-library/lesson1-practice/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+      <div>
+        <h2>Another component</h2>
+       <p>Vania</p>
+      </div>
+    );
+  }
+  export default Test;
