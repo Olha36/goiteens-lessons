131b5bdbcb09e051d7300771e78cb7b50a4215c2
olha36 <olhakucheruk36@gmail.com>
2025-05-10 11:40:04 +0300

    loops lesson 1

---
+++ 3semester/lesson6-loops/script.js
@@ -0,0 +1,28 @@
+// while виконується якщо і поки умова правдива
+// let a = 5;
+// while(a < 5) {
+//     a++;
+    
+//     console.log(a)
+// }
+
+// for виконується на кожній ітерації кілька раз
+// let b = [1, 2, 3, 4, 5];
+// for (let i = 0; i  <= b.length; i++) {
+//     console.log(b[i]);
+   
+// }
+// console.log(b);
+
+// do...while виконається хоча б 1 раз, навіть якщо умва хибна
+// let number = 10;
+// do {
+//     console.log(number);
+//     number++;
+// } while (number <= 15)  // правдива умова
+
+// do {
+//     console.log(number);
+//     number--;
+     
+// } while (number >= 3)
