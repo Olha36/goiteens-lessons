132fbb52d73c5a70ab9111f22aa8c6fa4f5a5f56
olha36 <olhakucheruk36@gmail.com>
2025-05-20 19:27:13 +0300

    arrays tasks

---
+++ 3semester/lesson7/arrays.js
@@ -0,0 +1,39 @@
+// const arr = ['hello', 'world', 1, 2, 3, 4, 5, true];
+// console.log(arr[4]);
+// arr[1] = 'goiteens';
+
+// console.log(arr[1]);
+// console.log(arr.length);
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+
+// }
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// console.log(matrix[1][0]); // 4
+// console.log(matrix[1][1]); // 5
+
+// task 1
+// let Arr1 = [1, 5, '4', "hello"];
+// let Arr2 = [true,2, {}, ['a'], 222];
+
+// console.log(Number(Arr1[2]) + Arr2[1]);
+
+// 3
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+let total = 0;
+
+// for (let i = 0; i < cart.length; i++) {
+//     console.log(cart[i]);
+
+// }
+for (let item of cart) {
+  total += item;
+  
+}
+
+console.log(total);
